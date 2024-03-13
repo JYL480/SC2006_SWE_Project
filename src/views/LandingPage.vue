f56252f1f6c086a128db4a38b6b28b5081b5b62f
YuQuanang <angyuquan12@gmail.com>
2024-03-13 12:43:45 +0800

    13/3/24 Added the SearchBar.vue component design.

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,7 +1,10 @@
 <template>
     Landing page here
+    
     <navbar></navbar>
+
 </template>
+
 <script setup>
 // This page is the home default page!!
     import navbar from '../components/NavBar.vue';
