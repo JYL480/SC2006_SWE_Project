46948f06a72b0dc2c96fb39e954b8d2212c16a98
JYL480 <leejunyang480@gmail.com>
2024-03-15 14:26:16 +0800

    15/3/2024 - Added the map component - And imported into the LandingPage thing - Still need to work on this

---
+++ src/components/Map.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="map-container"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { MapboxMap, MapboxGeolocateControl } from "@studiometa/vue-mapbox-gl";
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+const mapCenter = ref([103.82287200000002, 1.3649170000000002]);
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibGp5NDgwIiwiYSI6ImNsdGY4a2F1MjBtNzEyam45MzV5bXl1NG8ifQ.e1MgohLQEySFfrautJ_7lQ";
+
+onMounted(() => {
+  const map = new mapboxgl.Map({
+    container: "map-container", // container ID
+    center: mapCenter.value, // starting position [lng, lat]
+    style: "mapbox://styles/ljy480/cltfztv7d00ub01nw3uhsceke/draft",
+    zoom: 10, // starting zoom
+  });
+});
+</script>
+
+<style>
+#map-container {
+  width: 100%;
+  height: 500px;
+}
+.map-container {
+  flex: 1;
+}
+</style>
