aee98a13f832572ae502c389a9be77ab7977fe4f
Totoroxdxd <assignmentsforelaine@gmail.com>
2024-03-31 04:38:30 +0800

    Added Summary SideBar template

--- src/components/SummarySideBar.vue
+++ src/components/SummarySideBar.vue
@@ -4,20 +4,52 @@
   <nav class="sidebar" :class="{close:istoggle}">
     <header>
       <div class="image-text">
-        <span class="image">
-          <img src="logo.png" alt="">
-        </span>
-
         <div class="text logo-text">
-          <span class="name">Siam Fees</span>
-          <span class="profession">Your one stop carpark and ERP finding platform</span>
+          <div class="SortOrderControl">
+            <span class="SortOrderControl__option --isActive">Distance</span>
+            <span class="SortOrderControl__option ">Price</span>
+            <span class="SortOrderControl__option ">Relevance</span>
+          </div>
         </div>
       </div>
-      <i class='toggle' @click="toggleButton"></i>
+      <i class="toggle" @click="toggleButton"></i>
     </header>
 
     <div class="menu-bar">
       <div class="menu">
+
+        <div v-show="!istoggle" class="card">
+            <div class="card-details">
+                <div class="locationBox">
+                    <div class="location"> location </div> 
+                    <div class="slotsBox"> slots </div>           
+                </div>
+                <div class="messageBox1"> 
+                    <span><b>Price</b></span>
+                    <br>
+                    <span>$12</span>
+
+                </div>
+                <div v-show=false class="messageBox2"> 
+                    <span><b>Operating Hours</b></span>
+                    <br>
+                    <span>10am-7pm</span>
+                </div>
+
+                <div class="messageBox3"> 
+                    <span><b>Slots</b></span>
+                    <br>
+                    <span>7</span>
+                </div>
+
+                
+                    
+            </div>
+        </div>
+        
+
+
+
         <!--
         <ul class="menu-links">
           <li class="nav-link">
@@ -131,7 +163,7 @@ function toggleButton(){
   --primary-color: #695cfe;
   --primary-color-light: #f6f5ff;
   --toggle-color: #ddd;
-  --text-color: #e4e9f7;
+  --text-color: #7f8081;
 
   /* ====== Transition ====== */
   --tran-03: all 0.2s ease;
@@ -152,19 +184,9 @@ function toggleButton(){
   padding: 10px 14px;
   background: var(--sidebar-color);
   transition: var(--tran-05);
-  z-index: 100;
 }
 .sidebar.close {
-    position: fixed;
-  top: 0;
   right: -350px;
-  height: 100%;
-  width: 400px;
-  overflow-y: hidden;
-  padding: 10px 14px;
-  background: var(--sidebar-color);
-  transition: var(--tran-05);
-  z-index: 100;
 }
 
 /* ===== Reusable code - Here ===== */
@@ -222,8 +244,120 @@ function toggleButton(){
   flex-direction: column;
 }
 
+.SortOrderControl {
+    padding-left: 45px;
+    color: #ddd;
+    font-size: 1.6rem;
+}
+
+.SortOrderControl__option {
+    display: inline-block;
+    padding: 4px 16px;
+    border-right: 1px solid #ddd;
+    cursor: pointer;
+}
+
+.SortOrderControl__option.\--isActive {
+    color: #2963a3;
+    cursor: default;
+}
+/*
+.LocationListItem {
+    box-shadow: 0 2px 5px 0 rgba(0,0,0,.3);
+    border-radius: 4px;
+    display: block;
+    width: 100%;
+    background-color: #fff;
+    position: relative;
+    margin-bottom: 8px;
+    transition: all .1s ease-in-out;
+}
+
+.LocationListItem__containerLink {
+    display: block;
+    color: #555;
+    text-decoration: none;
+    cursor: pointer;
+    zoom: 1;
+}
+
+.LocationListItem__title {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    margin: 0;
+    color: #fff;
+    text-align: left;
+    font-size: 1.8rem;
+    font-family: museo_sans, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;
+    -ms-flex: 1;
+    flex: 1;
+}
+
+.LocationListItem__containerLink:before {
+    content: " ";
+    display: table;
+}
+
+.LocationListItem__titleBar {
+    border-radius: 4px 4px 0 0;
+    padding: 6px 12px;
+    width: 100%;
+    background: #2963a3;
+    align-items: center;
+}
+
+.LocationListItem__titleBar.\--offStreet {
+    background: #2963a3;
+}
+
+.LocationListItem__capacity {
+    padding-left: 16px;
+    color: #fff;
+    font-size: 1.4rem;
+    line-height: 1;
+}
+
+.LocationListItem__searchDetails {
+    margin: 8px 0;
+    margin-top: 8px;
+    margin-right: 0px;
+    margin-bottom: 8px;
+    margin-left: 0px;
+}
+
+.LocationDetailsSearchDetails {
+    display: -ms-flexbox;
+    display: flex;
+}
+
+.LocationDetailsSearchDetails__detail {
+    -ms-flex: 1;
+    flex: 1;
+    text-align: center;
+}
+
+.LocationDetailsSearchDetails__detail__value {
+    font-size: 3rem;
+}
+
+.LocationDetailsSearchDetails__detail__label {
+    display: block;
+    font-size: 1.6rem;
+}
+
+.LocationDetailsSearchDetails__detail__value.\--small {
+    font-size: 1.8rem;
+}
+
+.LocationDetailsSearchDetails__detail__label {
+    display: block;
+    font-size: 1.6rem;
+}*/
+
 
 header .image-text .name {
+  padding-left: 60px;
   margin-top: 2px;
   font-size: 30px;
   font-weight: 600;
@@ -356,4 +490,92 @@ body.dark .mode .sun-moon i.moon {
   background-color: var(--toggle-color);
   transition: var(--tran-05);
 }
+
+/* For ListItens using cards */
+.card {
+ width: 395px;
+ height: 130px;
+ border-radius: 20px;
+ background: #f5f5f5;
+ position: relative;
+ border: 2px solid #c3c6ce;
+ transition: 0.5s ease-out;
+ overflow: visible;
+}
+.locationBox {
+ width: 392px;
+ height: 35px;
+ border-radius: 20px 20px 0px 0px;
+ background: #219ebc;
+ position: relative;
+ border: 2px solid #c3c6ce;
+ 
+}
+
+.card-details {
+ color: black;
+ height: 100%;
+ gap: .5em;
+ place-content: center;
+}
+
+/*Hover*/
+.card:hover {
+ border-color: #008bf8;
+ box-shadow: 0 4px 18px 0 rgba(0, 0, 0, 0.25);
+}
+
+.messageBox1 {
+    box-sizing: border-box;
+    float: left;
+    width: 50%; 
+    padding: 20px; /* if you want space between the images */
+    text-align: center;
+    margin-block-end: 1em;
+
+}
+
+.messageBox2 {
+    box-sizing: border-box;
+    float: left;
+    width: 50%; 
+    padding: 20px; /* if you want space between the images */
+    text-align: center;
+    border-left: thick double #219ebc;
+}
+
+.messageBox3 {
+    box-sizing: border-box;
+    float: left;
+    width: 50%; 
+    padding: 20px; /* if you want space between the images */
+    text-align: center;
+    border-left: thick double #219ebc;
+}
+
+.messageBox3 {
+    box-sizing: border-box;
+    float: left;
+    width: 50%; 
+    padding: 20px; /* if you want space between the images */
+    text-align: center;
+    border-left: thick double #219ebc;
+}
+
+.location {
+    box-sizing: border-box;
+    float: left;
+    width: 80%; 
+    padding: 5px; /* if you want space between the images */
+    overflow: hidden;
+}
+
+.slotsBox {
+    box-sizing: border-box;
+    float: left;
+    width: 20%; 
+    padding: 5px; /* if you want space between the images */
+}
+
+
 </style>
